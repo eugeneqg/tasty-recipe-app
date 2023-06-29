d759534ea5249fcf7be7098ce23114c66c581f79
eugeneqg <evgeniisypchenko@gmail.com>
2023-06-29 12:43:06 +0400

    check

--- src/pages/recipe-page.js
+++ src/pages/recipe-page.js
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-// import RecipeCard from "../components/recipe-card/recipe-card";
 import { useParams } from "react-router-dom";
 import React from "react";
 import GetData from "../services/services";
